ec6e5a0105a1cf58c2d23c91a18ee73112b9e166
Andrei Astanei <andreihao@gmail.com>
2019-07-30 14:47:13 +0300

    Modele de reprezentare a rezultatelor returnare de query-urile JPA Criteria API

---
+++ src/main/java/group/msg/examples/jpa/entity/day17/FifthTestModel.java
@@ -0,0 +1,15 @@
+package group.msg.examples.jpa.entity.day17;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FifthTestModel {
+    private String cityName;
+    private long numberOfStudents;
+}
---
+++ src/main/java/group/msg/examples/jpa/entity/day17/ForthTestModel.java
@@ -0,0 +1,13 @@
+package group.msg.examples.jpa.entity.day17;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ForthTestModel {
+    private String lastName;
+    private String firstName;
+    private double gradeAverage;
+}
