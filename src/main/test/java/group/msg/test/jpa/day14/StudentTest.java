d74d96bd12dcb184ed91f791ec48dc757811d162
leucul <larisa.leucuta@yahoo.com>
2019-07-30 14:40:31 +0300

    Criteria API

--- src/main/test/java/group/msg/test/jpa/day14/StudentTest.java
+++ src/main/test/java/group/msg/test/jpa/day14/StudentTest.java
@@ -16,6 +16,10 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
 import javax.transaction.*;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
@@ -44,20 +48,20 @@ public class StudentTest extends JPABaseTest {
 
     @Test
     public void studentsInACity() {
-        TypedQuery<StudentEntity> location = em.createQuery("select se.first_name from StudentEntity se where se.address.city like 'Timisoara'", StudentEntity.class);
+        TypedQuery<Student> location = em.createQuery("select se.first_name from Student se where se.address.city like 'Timisoara'", Student.class);
         Assert.assertEquals("Entities not found in the database!", 2, location.getResultList().size());
         System.out.println("Students that live in this city are: " + location.getResultList().toString());
     }
 
     @Test
     public void studentsCertainSubject() {
-        TypedQuery<StudentEntity> subject = em.createQuery("select distinct stud.first_name from StudentEntity stud join fetch stud.subjectList sub where sub.name='Mate'", StudentEntity.class);
+        TypedQuery<Student> subject = em.createQuery("select distinct stud.first_name from StudentEntity stud join fetch stud.subjectList sub where sub.name='Mate'", Student.class);
         System.out.println("Students with this subject are: " + subject.getResultList().toString());
     }
 
     @Test
     public void averageGrade() {
-        TypedQuery<StudentEntity> average = em.createQuery("select stud.first_name, avg(grade.grade) from StudentEntity stud join fetch stud.grade grade group by stud.first_name", StudentEntity.class);
+        TypedQuery<Student> average = em.createQuery("select stud.first_name, avg(grade.grade) from StudentEntity stud join fetch stud.grade grade group by stud.first_name", Student.class);
 
         List response = average.getResultList();
         response.forEach(r -> {
@@ -70,7 +74,7 @@ public class StudentTest extends JPABaseTest {
 
     @Test
     public void averageGradeAboveEight() {
-        TypedQuery<StudentEntity> average = em.createQuery("select stud.first_name, avg(grade.grade) from StudentEntity stud join fetch stud.grade grade group by stud.first_name having avg(grade.grade) > 8", StudentEntity.class);
+        TypedQuery<Student> average = em.createQuery("select stud.first_name, avg(grade.grade) from StudentEntity stud join fetch stud.grade grade group by stud.first_name having avg(grade.grade) > 8", Student.class);
 
         List response = average.getResultList();
         response.forEach(r -> {
@@ -81,10 +85,9 @@ public class StudentTest extends JPABaseTest {
         });
     }
 
-
     @Test
     public void countStudentsInCity() {
-        TypedQuery<StudentEntity> count = em.createQuery("select  stud.address.city, count(stud.first_name) from StudentEntity stud group by stud.address.city", StudentEntity.class);
+        TypedQuery<Student> count = em.createQuery("select  stud.address.city, count(stud.first_name) from StudentEntity stud group by stud.address.city", Student.class);
 
         List response = count.getResultList();
         response.forEach(r -> {
@@ -95,16 +98,110 @@ public class StudentTest extends JPABaseTest {
         });
     }
 
+    /****************************************************/
+
+
+    @Test
+    public void studentsInACityAPI() {
+
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery query = builder.createQuery(Student.class);
+
+        Root<Student> studentEntity = query.from(em.getMetamodel().entity(Student.class));
+
+        Join<Student, Address> address = studentEntity.join(Student_.address);
+
+        CriteriaQuery studentSelect = query.select(studentEntity.get(Student_.first_name));
+        studentSelect.where(builder.like(address.get("city"), "Timisoara"));
+
+        List<Object> result = em.createQuery(query).getResultList();
+
+        for (Object s : result) {
+            System.out.println("Students that live in this city: " + s);
+        }
+    }
+
+    @Test
+    public void studentsCertainSubjectAPI() {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery query = builder.createQuery(Student.class);
+
+        Root<Student> studentEntity = query.from(em.getMetamodel().entity(Student.class));
+
+        Join<Student, Subject> subject = studentEntity.join(Student_.subjectList);
+
+        CriteriaQuery studentSelect = query.select(studentEntity.get(Student_.first_name));
+        studentSelect.where(builder.like(subject.get("name"), "Mate"));
+
+        List<Object> result = em.createQuery(query).getResultList();
+
+        for (Object s : result) {
+            System.out.println("Students with this subject are: " + s);
+        }
+    }
+
+    @Test
+    public void averageGradeAPI() {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Object[]> query = builder.createQuery(Object[].class);
+
+        Root<Student> studentEntity = query.from(em.getMetamodel().entity(Student.class));
+
+        Join<Student, Grade> grade = (Join<Student, Grade>) studentEntity.fetch(Student_.grade);
+
+        query.multiselect(studentEntity.get(Student_.first_name), builder.avg(grade.get("grade"))).groupBy(studentEntity.get(Student_.first_name));
+
+        List<Object[]> result = em.createQuery(query).getResultList();
+        for (Object[] s : result) {
+            System.out.println(s[0] + "'s average is " + s[1]);
+        }
+    }
+
+    @Test
+    public void averageAboveEightAPI() {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery query = builder.createQuery();
+
+        Root<Student> studentEntity = query.from(em.getMetamodel().entity(Student.class));
+
+        Join<Student, Grade> grade = (Join<Student, Grade>) studentEntity.fetch(Student_.grade);
+
+        query.multiselect(studentEntity.get(Student_.first_name), builder.avg(grade.get(Grade_.grade))).having(builder.greaterThan(builder.avg(grade.get(Grade_.grade)), 8.0)).groupBy(studentEntity.get(Student_.first_name));
+
+        List<Object[]> result = em.createQuery(query).getResultList();
+        for (Object[] s : result) {
+            System.out.println(s[0] + "'s average is " + s[1]);
+        }
+    }
+
+    @Test
+    public void countStudentsAPI() {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery query = builder.createQuery();
+
+        Root<Student> studentEntity = query.from(em.getMetamodel().entity(Student.class));
+
+        Join<Student, Address> address = (Join<Student, Address>) studentEntity.fetch(Student_.address);
+
+        query.multiselect(address.get(Address_.city), builder.count(studentEntity.get(Student_.first_name))).groupBy(address.get(Address_.city));
+
+        List<Object[]> result = em.createQuery(query).getResultList();
+        for (Object[] s : result) {
+            System.out.println(s[0].toString() + " has " + s[1].toString() + " students ");
+        }
+    }
+
+    /*****************************************************/
 
     @Test
     public void createStudentEntity() {
-        Query q = em.createNativeQuery("SELECT * FROM STUDENT_ENTITY");
+        Query q = em.createNativeQuery("SELECT * FROM STUDENT");
         Assert.assertEquals("Entities not found in the database!", 3, q.getResultList().size());
     }
 
     @Test
     public void findStudentEntity() {
-        Query q = em.createNativeQuery("SELECT * FROM STUDENT_ENTITY WHERE FIRST_NAME ='Larisa'");
+        Query q = em.createNativeQuery("SELECT * FROM STUDENT WHERE FIRST_NAME ='Larisa'");
         Assert.assertEquals("Entities not found in the database!", 1, q.getResultList().size());
     }
 
@@ -112,45 +209,33 @@ public class StudentTest extends JPABaseTest {
     public void deleteStudentEntity() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
         utx.begin();
         em.joinTransaction();
-        StudentEntity studentEntity = em.find(StudentEntity.class, 50);
-        em.remove(studentEntity);
+        Student student = em.find(Student.class, 50);
+        em.remove(student);
         utx.commit();
 
         em.clear();
     }
 
+    /*********************************************************/
+
     @Test
     public void validateName() throws Exception {
         utx.begin();
         em.joinTransaction();
-        AdressEmbeddableEntity adressEmbeddableEntity = new AdressEmbeddableEntity();
-        adressEmbeddableEntity.setCountry("Romania");
-        adressEmbeddableEntity.setStreet("Lunga");
-        adressEmbeddableEntity.setCity("Timisoara");
+        Address address = createAddress("Romania", "Lunga", "Timisoara");
 
-        UniversityEntity universityEntity = new UniversityEntity();
-        universityEntity.setCountry("Romania");
-        universityEntity.setName("Poli");
+        University university = createUniversity("Romania", "Politehnica");
+        em.persist(university);
 
-        em.persist(universityEntity);
+        List<Subject> subjects = new ArrayList<>();
+        Subject subject = createSubject("Romana");
+        subjects.add(subject);
 
-        List<SubjectEntity> subjectEntities = new ArrayList<>();
-        SubjectEntity subject = new SubjectEntity();
-        subject.setName("Romana");
-        subjectEntities.add(subject);
-
-        for (SubjectEntity sub : subjectEntities) {
+        for (Subject sub : subjects) {
             em.persist(sub);
         }
 
-        StudentEntity s = new StudentEntity();
-        s.setAddress(adressEmbeddableEntity);
-        s.setEmail("alex@gmail.com");
-        s.setFirst_name("jf");
-        s.setLast_name("Gheorghe");
-        s.setUniversity_id(universityEntity);
-        s.setSection("Mec");
-        s.setSubjectList(subjectEntities);
+        Student s = createStudent(address, "alex.gheorghe@gmail.com", "Alex", "Gheorghe", university, "Section1", subjects);
 
         try {
             em.persist(s);
@@ -167,35 +252,19 @@ public class StudentTest extends JPABaseTest {
     public void validateAddress() throws Exception {
         utx.begin();
         em.joinTransaction();
-        AdressEmbeddableEntity adressEmbeddableEntity = new AdressEmbeddableEntity();
-        adressEmbeddableEntity.setCountry("Romania");
-        adressEmbeddableEntity.setStreet("Lunga");
-        adressEmbeddableEntity.setCity("Timisoara");
+        Address address = createAddress("Romania", "Lunga", "Timisoara");
 
-        UniversityEntity universityEntity = new UniversityEntity();
-        universityEntity.setCountry("Romania");
-        universityEntity.setName("Poli");
+        University university = createUniversity("Romania", "Politehnica");
+        em.persist(university);
 
-        em.persist(universityEntity);
+        List<Subject> subjects = new ArrayList<>();
+        Subject subject = createSubject("Romana");
+        subjects.add(subject);
 
-
-        List<SubjectEntity> subjectEntities = new ArrayList<>();
-        SubjectEntity subject = new SubjectEntity();
-        subject.setName("Romana");
-        subjectEntities.add(subject);
-
-        for (SubjectEntity sub : subjectEntities) {
+        for (Subject sub : subjects) {
             em.persist(sub);
         }
-
-        StudentEntity s = new StudentEntity();
-        s.setAddress(adressEmbeddableEntity);
-        s.setEmail("alex@gmail.com");
-        s.setFirst_name(null);
-        s.setLast_name("Gheorghe");
-        s.setUniversity_id(universityEntity);
-        s.setSection("Mec");
-        s.setSubjectList(subjectEntities);
+        Student s = createStudent(address, "alex.gheorghe@gmail.com", "Alex", "Gheorghe", university, "Section1", subjects);
 
 
         try {
@@ -209,135 +278,115 @@ public class StudentTest extends JPABaseTest {
         }
     }
 
+    /*********************************************************/
+
+    public Address createAddress(String city, String country, String street) {
+        Address addressEmbeddableEntity = new Address();
+        addressEmbeddableEntity.setCity(city);
+        addressEmbeddableEntity.setCountry(country);
+        addressEmbeddableEntity.setStreet(street);
+        return addressEmbeddableEntity;
+    }
+
+    public University createUniversity(String country, String name) {
+        University university = new University();
+        university.setCountry(country);
+        university.setName(name);
+        return university;
+    }
+
+    public Subject createSubject(String name) {
+        Subject subject = new Subject();
+        subject.setName(name);
+        return subject;
+    }
+
+    public Student createStudent(Address address, String email, String firstName, String lastName, University university, String section, List<Subject> subjectList) {
+        Student student = new Student();
+        student.setAddress(address);
+        student.setEmail(email);
+        student.setFirst_name(firstName);
+        student.setLast_name(lastName);
+        student.setUniversity_id(university);
+        student.setSection(section);
+        student.setSubjectList(subjectList);
+        return student;
+    }
+
+    public Grade createGrade(int grade, Subject subject, Student student) {
+        Grade grade1 = new Grade();
+        grade1.setGrade(grade);
+        grade1.setSubject(subject);
+        grade1.setStudent(student);
+        return grade1;
+    }
+
+    /***********************************************************/
 
     @Override
     protected void insertData() throws Exception {
         utx.begin();
         em.joinTransaction();
-        System.out.println("Inserting records...");
 
-        AdressEmbeddableEntity addressEmbeddableEntity = new AdressEmbeddableEntity();
-        addressEmbeddableEntity.setCity("Timisoara");
-        addressEmbeddableEntity.setCountry("Romania");
-        addressEmbeddableEntity.setStreet("Ion Creanga");
+        Address address1 = createAddress("Timisoara", "Romania", "Ion CReanga");
+        Address address2 = createAddress("Cluj", "Romania", "Titulescu");
 
-        AdressEmbeddableEntity addressEmbeddableEntity1 = new AdressEmbeddableEntity();
-        addressEmbeddableEntity1.setCity("Cluj");
-        addressEmbeddableEntity1.setCountry("Romania");
-        addressEmbeddableEntity1.setStreet("Titulescu");
+        University university = createUniversity("Romania", "Politehnica");
 
-        UniversityEntity universityEntity = new UniversityEntity();
-        universityEntity.setCountry("Romania");
-        universityEntity.setName("Politehnica");
+        List<Subject> subject1 = new ArrayList<>();
+        List<Subject> subject2 = new ArrayList<>();
+        Subject info = createSubject("Info");
+        Subject mate = createSubject("Mate");
+        Subject romana = createSubject("Romana");
+        subject1.add(info);
+        subject1.add(mate);
+        subject2.add(romana);
 
+        List<Student> students = new ArrayList<>();
+        Student student1 = createStudent(address1, "larisa.leucuta@gmail.com", "Larisa", "Leucuta", university, "Section1", subject1);
+        Student student2 = createStudent(address1, "andrei@gmail.com", "Andrei", "Astanei", university, "Section1", subject1);
+        Student student3 = createStudent(address2, "alex.gheorghe@gmail.com", "Alex", "Gheorghe", university, "Section2", subject2);
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
 
-        List<SubjectEntity> subjectEntities = new ArrayList<>();
-
-        SubjectEntity info = new SubjectEntity();
-        info.setName("Info");
-
-        SubjectEntity mate = new SubjectEntity();
-        mate.setName("Mate");
-
-
-        List<SubjectEntity> subjectEntities1 = new ArrayList<>();
-        SubjectEntity romana = new SubjectEntity();
-        romana.setName("Romana");
-
-        subjectEntities.add(info);
-        subjectEntities.add(mate);
-        subjectEntities1.add(romana);
-
-
-        List<StudentEntity> studentEntities = new ArrayList<>();
-
-        StudentEntity student = new StudentEntity();
-        student.setAddress(addressEmbeddableEntity);
-        student.setEmail("larisa.leucuta@yahoo.com");
-        student.setFirst_name("Larisa");
-        student.setLast_name("Leucuta");
-        student.setUniversity_id(universityEntity);
-        student.setSection("Section1");
-        student.setSubjectList(subjectEntities);
-        student.setId(50);
-        studentEntities.add(student);
-
-        StudentEntity student1 = new StudentEntity();
-        student1.setAddress(addressEmbeddableEntity1);
-        student1.setEmail("andrei@gmail.com");
-        student1.setFirst_name("Andrei");
-        student1.setLast_name("Astanei");
-        student1.setUniversity_id(universityEntity);
-        student1.setSection("Section1");
-        student1.setSubjectList(subjectEntities);
-        studentEntities.add(student1);
-
-        StudentEntity student2 = new StudentEntity();
-        student2.setAddress(addressEmbeddableEntity);
-        student2.setEmail("alex@gmail.com");
-        student2.setFirst_name("Alex");
-        student2.setLast_name("Gheorghe");
-        student2.setUniversity_id(universityEntity);
-        student2.setSection("Section2");
-        student2.setSubjectList(subjectEntities1);
-        studentEntities.add(student2);
-
-        List<GradeEntity> grades = new ArrayList<>();
-
-        GradeEntity grade = new GradeEntity();
-        grade.setGrade(10);
-        grade.setSubject(info);
-        grade.setStudentEntity(student);
-
-        GradeEntity grade1 = new GradeEntity();
-        grade1.setGrade(9);
-        grade1.setSubject(mate);
-        grade1.setStudentEntity(student1);
-
-        GradeEntity grade2 = new GradeEntity();
-        grade2.setGrade(8);
-        grade2.setSubject(romana);
-        grade2.setStudentEntity(student2);
-
-        GradeEntity grade3 = new GradeEntity();
-        grade3.setGrade(5);
-        grade3.setSubject(mate);
-        grade3.setStudentEntity(student);
-
-        grades.add(grade);
+        List<Grade> grades = new ArrayList<>();
+        Grade grade1 = createGrade(10, info, student1);
+        Grade grade2 = createGrade(9, mate, student1);
+        Grade grade3 = createGrade(6, romana, student1);
+        Grade grade4 = createGrade(9, info, student2);
+        Grade grade5 = createGrade(10, romana, student3);
+        Grade grade6 = createGrade(7, mate, student2);
         grades.add(grade1);
         grades.add(grade2);
         grades.add(grade3);
+        grades.add(grade4);
+        grades.add(grade5);
+        grades.add(grade6);
 
-        student.setGrade(grades);
         student1.setGrade(grades);
         student2.setGrade(grades);
+        student3.setGrade(grades);
 
-        em.persist(universityEntity);
+        em.persist(university);
 
-        for (SubjectEntity sub : subjectEntities) {
+        for (Subject sub : subject1) {
             em.persist(sub);
         }
-        for (SubjectEntity sub : subjectEntities1) {
+        for (Subject sub : subject2) {
             em.persist(sub);
         }
 
-        for (StudentEntity s : studentEntities) {
+        for (Student s : students) {
             em.persist(s);
         }
-
-//        for (GradeEntity g : grades) {
-//            em.persist(g);
-//        }
-
         utx.commit();
-
         em.clear();
     }
 
     @Override
     protected void internalClearData() {
-        em.createQuery("delete from StudentEntity").executeUpdate();
+        em.createQuery("delete from Student").executeUpdate();
     }
 
 
